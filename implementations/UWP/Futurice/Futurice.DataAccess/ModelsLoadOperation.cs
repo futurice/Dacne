914e8b599ca988b38ee5a1f05dd3efa0dd815f86
Jarno Montonen <jarno.montonen@futurice.com>
2015-11-20 17:32:53 +0200

    Removed some now redundant classes.

--- implementations/UWP/Futurice/Futurice.DataAccess/ModelsLoadOperation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Futurice.DataAccess
-{
-    public class ModelsLoadOperation : Operation<object>
-    {
-        public ModelsLoadOperation(Func<IObservable<OperationState<object>>> begin) : base(begin) { }
-    }
-}
