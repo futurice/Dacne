1b4359a3eb728b39e4de54af5fbd7b11ad5da3d9
Jarno Montonen <jarno.montonen@futurice.com>
2016-01-27 18:05:17 +0200

    Wip

---
+++ implementations/UWP/Futurice/Futurice.DataAccess/DataLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace Futurice.DataAccess
+{
+    public abstract class DataLoader
+    {
+        public DataLoader(Uri baseUri)
+        {
+
+        }
+
+        protected abstract IObservable<OperationState<IBuffer>> LoadImplementation(ModelIdentifier id, ModelSource source);
+
+    }
+
+}
--- implementations/UWP/Futurice/SampleApplication/ModelLoader.cs
+++ implementations/UWP/Futurice/SampleApplication/ModelLoader.cs
@@ -18,15 +18,7 @@ namespace SampleApplication
         protected override IObservable<OperationState<IBuffer>> LoadImplementation(ModelIdentifier id, ModelSource source)
         {
             // Check that this model is supposed to be loaded from the bbc
-            if (source == ModelSource.Disk) {                
-                return Observable.Generate(0, p => p <= 100, p => ++p,
-                    p => new OperationState<IBuffer>(p == 100 ? Encoding.UTF8.GetBytes("Article from disk").AsBuffer() : null, p),
-                    p => TimeSpan.FromMilliseconds(1));
-            } else {
-                return Observable.Generate(0, p => p <= 100, p => ++p,
-                    p => new OperationState<IBuffer>(p == 100 ? Encoding.UTF8.GetBytes("Article from server").AsBuffer() : null, p),
-                    p => TimeSpan.FromMilliseconds(50));
-            }
+            return new BbcDataLoader(new Uri("http://www.bbc.com/api")).Load(id, source);
         }
 
         protected override IObservable<OperationState<object>> ParseImplementation(ModelIdentifier id, IBuffer data)
@@ -36,6 +28,22 @@ namespace SampleApplication
                 new OperationState<object>(
                     new NewsArticle() { Title = Encoding.UTF8.GetString(data.ToArray()) }, 100)
                 );
+        }
+    }
+
+    public static DataLoader
+    {
+        public IObservable<OperationState<IBuffer>> Load(ModelIdentifier id, ModelSource source)
+        {
+            if (source == ModelSource.Disk) {
+                return Observable.Generate(0, p => p <= 100, p => ++p,
+                    p => new OperationState<IBuffer>(p == 100 ? Encoding.UTF8.GetBytes("Article from disk").AsBuffer() : null, p),
+                    p => TimeSpan.FromMilliseconds(1));
+            } else {
+                return Observable.Generate(0, p => p <= 100, p => ++p,
+                    p => new OperationState<IBuffer>(p == 100 ? Encoding.UTF8.GetBytes("Article from server").AsBuffer() : null, p),
+                    p => TimeSpan.FromMilliseconds(50));
+            }
         }
     }
 }
